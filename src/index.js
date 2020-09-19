2ac398b35d13eaeee4efc3a42e9b9710567fd767
Constantin Ginga <costelgn@gmail.com>
2020-09-19 14:51:04 +0200

    Add input formatting functions

--- src/index.js
+++ src/index.js
@@ -2,26 +2,51 @@ import {
     todoFactory,
     formatUserInput
 } from './todos';
+import {
+    generateInstructions,
+    generateError,
+    clearScreen
+} from './displayController';
+import {
+    projectFactory
+} from './projects';
 import './style.css';
 
-let todo1 = todoFactory('test', 'asdasdas', '2020', 'important');
+
+const inputBox = document.querySelector("#todo-input");
+const parent = document.querySelector('#instructions');
+
+inputBox.addEventListener('keydown', newTodo);
 
 
-const inputBox = document.querySelector("#todo-txt");
-
-inputBox.addEventListener('keypress', newTodo);
-
+const projects = [projectFactory('inbox')];
 
 function newTodo(e) {
     if (e.key.toLowerCase() === 'enter') {
-        // format user string (function)
-        let userInput = formatUserInput(inputBox.value);
-        if (userInput === undefined) {
-            console.log('Error. Incorrect usage.');
-        } else {
-            console.log(userInput);
-        }
-        // create new todo object (function)
 
+        let input = inputBox.value.trim();
+        const indexOfArgs = input.indexOf('--');
+        inputBox.value = '';
+
+        if (input === 'doc') {
+            generateInstructions(parent);
+        } else if ( /*check for project folder */ false) {
+
+        } else {
+            input = formatUserInput(input, indexOfArgs);
+
+            if (input.includes('error')) {
+                generateError(input, parent);
+            } else if (indexOfArgs === -1 && input) {
+                projects[0].push(input);
+            } else {
+                inbox.push(todoFactory(input[0], input[1], input[2], input[3]));
+            }
+            console.log(inbox);
+        }
+    } else if (e.ctrlKey && e.key === 'l') {
+        // disable browser shortcut
+        e.preventDefault();
+        clearScreen(parent);
     }
 }
--- src/todos.js
+++ src/todos.js
@@ -1,16 +1,15 @@
+import moment from 'moment';
 export {
     todoFactory,
     formatUserInput
 };
 
 
-/* to-do: check for correct date format (use library)
-animate usage text with typewriter effect (use library), them autofocus on input
-make dollar sign just outside of input
-type inbox/, today/, etc. and use typewriter effect to show task in that project
-if already in that project, print error 
-think of a command to add projects
-as new projects are created, add them to sidebar*/
+/* to-do:
+    type inbox/, today/, etc. and use typewriter effect to show task in that project
+    think of a way to add task to a project
+    edit task_name fills input with task string, letting user to edit it.
+    as new projects are created/deleted, add/remove them to sidebar*/
 
 const todoFactory = (title, desc, dueDate, priority) => {
     return {
@@ -22,62 +21,91 @@ const todoFactory = (title, desc, dueDate, priority) => {
 }
 
 
-function formatUserInput(input) {
-    input = input.trim();
-    const indexOfArgs = input.indexOf('--');
+function formatUserInput(input, index) {
 
-    if (input[0] === '-' && input[1] === '-' || !input) {
-        return;
-    } else if (indexOfArgs === -1 && input) {
-        return [input];
-    } else return formatArgs(input, indexOfArgs);
-}
+    if (input[0] === '-' && input[1] === '-' || !input) return 'error, title is missing';
 
-
-function formatArgs(input, index) {
     // isolate args
     let argsString = input.slice(index);
-    // separate desc and title from args
-    const desc = argsString.substring(argsString.lastIndexOf('--')),
-        title = input.slice(0, index).trim();
-    // remove desc from args
-    argsString = argsString.replace(desc, '').trim();
+    // separate title from args
+    const title = input.slice(0, index).trim();
     // clean up args and split into array
-    let formattedInput = argsString.replace(/--/g, '').split(' ');
+    let formattedInput = argsString.split('--');
+    console.log(formattedInput);
+    // remove title
+    formattedInput.shift();
 
-    if (formattedInput.length > 2) return;
-
-    // order args in the correct order
-    orderArgs(formattedInput);
-
-    // add title to array start
-    formattedInput.unshift(title);
-    // add desc to array end
-    formattedInput.push(desc.replace('--', ''));
-    return formattedInput;
+    const check = checkArgs(formattedInput);
+    // if there is an error, return it
+    if (check.includes('error')) {
+        return check;
+    } else {
+        // sort args
+        if (formattedInput.length === 3) formattedInput = sortArgs(formattedInput);
+        // add title to array start
+        formattedInput.unshift(title);
+        return formattedInput;
+    }
 }
 
 
 
-function orderArgs(args) {
-    if (args.length != 1) {
-        for (let i = 0; i < args.length; i++) {
-            // remove white space from each arg
-            args[i] = args[i].trim();
+function checkArgs(args) {
 
-            if (args[i][0] === '#') {
+    // letters, numbers and spaces
+    const regex = /[A-Za-z0-9 _]*[A-Za-z0-9][A-Za-z0-9 _]*$/g,
+        count = {
+            priority: 0,
+            date: 0,
+            description: 0
+        };
 
-                // if there are too many hashtags
-                if (args[i].length > 3) return;
+    for (let i = 0; i < args.length; i++) {
+        args[i] = args[i].trim();
 
-                // if priority is second, reorder array
-                if (args.indexOf(args[i])) {
-                    [args[i - 1], args[i]] = [args[i], args[i - 1]];
-                }
+        if (!args[i]) args.splice(i, 1);
+
+        // check for correct argument format
+        if (args.length && args[i]) {
+            if ((args[i].includes('#') && args[i].length > 3) && !moment(args[i], 'DD.MM.YYYY', true).isValid() &&
+                !args[i].match(regex)) return 'error, wrong argument format';
+        }
+    }
+
+    if (args.length > 3) return 'error, number of arguments exceeded';
+
+    if (args.length === 3) {
+
+        // check for duplicates
+        for (let item of args) {
+            if (item.includes('#') && !item.match(regex)) {
+                count.priority++;
+            } else if (item.match(regex) && !moment(item, 'DD.MM.YYYY', true).isValid()) {
+                count.description++;
+            } else if (moment(item, 'DD.MM.YYYY', true).isValid()) {
+                count.date++;
             }
         }
-        // if array contains an empty string
-    } else if (!args[0]) {
-        args.pop();
+
+        for (let prop in count) {
+            if (!count[prop]) return `error, ${prop} is missing.`;
+        }
     }
+
+    return 'success';
+}
+
+
+
+function sortArgs(args) {
+
+    let sorted = [];
+
+    for (let item of args) {
+        if (item.includes('#')) sorted[0] = item
+        else if (moment(item, 'DD.MM.YYYY', true).isValid()) sorted[1] = item
+        else sorted[2] = item;
+    }
+
+    return sorted;
 }
