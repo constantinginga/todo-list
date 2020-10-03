3580f3611db8d5d21e8e78fedb0b73a62cbb20ea
Constantin Ginga <costelgn@gmail.com>
2020-10-03 16:14:28 +0200

    Implement view history feature with arrow keys

--- src/index.js
+++ src/index.js
@@ -17,15 +17,21 @@ import {
 import './style.css';
 
 
-const inputBox = document.querySelector("#todo-input");
+const inputBox = document.querySelector('#todo-input');
 const parent = document.querySelector('#instructions');
 inputBox.addEventListener('keydown', newInput);
 const projects = [projectFactory('inbox'), projectFactory('today'), projectFactory('upcoming')];
-
+let previousInputs = [],
+    keyPressed = 0;
 
 
 function newInput(e) {
     if (e.key.toLowerCase() === 'enter') {
+        clearScreen(parent);
+        // store previous inputs
+        if (inputBox.value) previousInputs.unshift(inputBox.value);
+        // reset array index
+        keyPressed = 0;
 
         let input = inputBox.value.trim();
         let existingProject = checkProjectName(projects, input);
@@ -35,8 +41,8 @@ function newInput(e) {
             generateInstructions(parent);
         } else if (input.slice(0, 3) === 'rm ') {
             removeItem(input, projects);
-            // check if a project with that name exists
         } else if (existingProject != null) {
+            // check if a project with that name already exists
             (typeof existingProject == 'string') ? generateError(existingProject, parent): generateTodos(existingProject, parent);
         } else {
             input = formatUserInput(input);
@@ -63,8 +69,19 @@ function newInput(e) {
 
         console.log(projects);
     } else if (e.ctrlKey && e.key === 'l') {
-        // disable browser shortcut
+        // disable default browser behavior
         e.preventDefault();
         clearScreen(parent);
+    } else if (previousInputs.length && (e.key === 'ArrowUp' || e.key === 'ArrowDown')) {
+        // prevent cursor from changing positions
+        e.preventDefault();
+
+        // determine current array index
+        if (e.key === 'ArrowUp' && keyPressed < previousInputs.length) keyPressed++
+        else if (e.key === 'ArrowDown' && keyPressed > 0 && keyPressed <= previousInputs.length) keyPressed--;
+
+        // show corresponding element
+        if (e.key === 'ArrowDown' && keyPressed === 0) inputBox.value = '';
+        else inputBox.value = previousInputs[keyPressed - 1];
     }
 }
